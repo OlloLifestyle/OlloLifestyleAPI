59644fed7531d472b96b2559249c2c5d3fac3377
DayanDev87 <Dev.thinkit@gmail.com>
2025-08-06 15:22:32 +0800

    Docker HUB Setup

--- OlloLifestyleAPI/Controllers/AuthController.cs
+++ OlloLifestyleAPI/Controllers/AuthController.cs
@@ -285,7 +285,7 @@ public class AuthController : ControllerBase
     {
         try
         {
-            return Ok("okay sample now work");
+            return Ok("okay sample now work with CICD");
         }
         catch (Exception ex)
         {
